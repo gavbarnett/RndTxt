04a8094af7a82c4f983499efb77039d9f6269ae8
Gavin Barnett <gavbarnett@gmail.com>
2018-05-29 21:01:34 +0100

    JC increments

--- assets/js/Author.js
+++ assets/js/Author.js
@@ -19,12 +19,37 @@ Joseph Campbell's - Hero Journey
     12 --> 0
 
 */
+var theworld = new World();
 var thestory = new Story();
 
 function Story () {
+    this.choices = []
+    this.results = []
+    this.newpara = ""
+    this.decision = []
+    this.nextline = function (choiceref) {
+        if (!choiceref) {
+            //begin the story. This is only run once
+            appendParagraphs ("Whether you shall turn out to be the hero of your life, or whether that station will be held by anybody else, these pages must show."); //Thank you David Copperfield
+            this.choices = ["Open your eyes"]
+            this.results = [{JC:1},{Bob:3}]
+            appendChoice (this.choices)
+        } else {
+            //continue the story. This is run every time except the first.
+            this.decision = this.results[this.choices.indexOf(choiceref)]
+            //updateworld
+            theworld.UpdateWorld(this.decision)
+            //construct new paragraph
+            this.newpara = theworld.JC_value
+            appendParagraphs(this.newpara)
+            appendChoice (this.choices)  
+        }
+    }
+}
+
+function World () {
     //model of the world
-    this.JC_value = 0; //Joseph Campbell value
-    this.choices = ["Open your eyes"]
+    this.JC_value = 0 //Joseph Campbell value
     this.location = "Forest"
     this.weather = "Sunny"
     var itemCount = 3;
@@ -33,48 +58,45 @@ function Story () {
         this.items.push(randItem())
     } 
     this.money = Math.round(Math.random()*100)
-    this.nextline = function (decision) {
-        if (!decision) {
-            appendParagraphs ("Whether you shall turn out to be the hero of your life, or whether that station will be held by anybody else, these pages must show."); //Thank you David Copperfield
-            appendChoice (this.choices)
-        } else {
-            appendParagraphs("You look around. You are lost in this " + this.location + ".")
-            appendChoice (this.choices)  
+    var updatepush = []
+    this.UpdateWorld = function(updates){
+        if (updates.JC){
+            this.JC_value += updates.JC
         }
-        
     }
 }
 
+
 function randItem () {
     var itemArray = [];
-    itemArray.push({
+    itemArray = [{
         name: "bread",
         edible: true,
         energy: 20+Math.floor(Math.random()*10),
         weight: 500+Math.floor(Math.random()*100),
         strength: 5+Math.floor(Math.random()*5)
-    });
-    itemArray.push({
+    },
+    {
         name: "stick",
         edible: false,
         energy: 0,
         weight: 400+Math.floor(Math.random()*200),
         strength: 50+Math.floor(Math.random()*100)
-    });
-    itemArray.push({
+    },
+    {
         name: "axe",
         edible: false,
         energy: 0,
         weight: 1000+Math.floor(Math.random()*700),
         strength: 300+Math.floor(Math.random()*200)
-    });
-    itemArray.push({
+    },
+    {
         name: "rope",
         edible: false,
         energy: 0,
         weight: 500+Math.floor(Math.random()*50),
         strength: 200+Math.floor(Math.random()*100)
-    });
+    }]
 
     return itemArray[Math.floor(Math.random()*itemArray.length)];
 }
